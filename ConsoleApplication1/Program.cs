884f4130616717245fc33afd8875f63877da006f
Leonid Malyshev <malyshev@streamstar.com>
2016-12-13 10:58:57 +0100

    tmp

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
-// https://codility.com/programmers/task//
+//https://leetcode.com/problems/remove-duplicate-letters/
+using System.Text;
+
+//leetcode.com/problems/remove-duplicate-letters/
 
 namespace ConsoleApplication1
 {
@@ -10,91 +14,36 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string inp = "54577";
+            string inp = "cbacdcbc";//acdb
             var s = new Solution();
-            var sw1 = new Stopwatch();
-            var sw2 = new Stopwatch();
-            sw1.Start();
-            var x1 = s.solution(inp);
-            sw1.Stop();
-            sw2.Start();
-            var x2 = s.solution2(inp);
-            sw2.Stop();
+            var x1 = s.RemoveDuplicateLetters(inp);
             Console.WriteLine(x1);
-            Console.WriteLine(x2);
-            Console.WriteLine(sw1.Elapsed+"    "+sw2.Elapsed);
             Console.ReadLine();
         }
     }
 
     public class Solution
     {
-        public int solution(string S)
+        public string RemoveDuplicateLetters(string s)
         {
-            var len = S.Length;
-            int count = 0;
-            var oddDict = new Dictionary<char,int>();
-            for (int k=0;k<10;k++)
-                oddDict.Add(k.ToString()[0],-1);
+            var dict= new Dictionary<char,int>();
+            var len = s.Length;
+            StringBuilder result= new StringBuilder();
             for (int i = 0; i < len; i++)
             {
-                oddDict.Clear();
-                for (int k = 0; k < 10; k++)
-                    oddDict.Add(k.ToString()[0], -1);
-                oddDict[S[i]] = 1;
-                int res = 0;
-                for (int j = i; j < len;)
+                var c = s[i];
+                if (!dict.ContainsKey(c))
                 {
-                    res += oddDict[S[j]] ;
-                    if (res <= 1)
-                    {
-                        if (res == (j - i + 1)%2)
-                        {
-                            count++;
-                        }
-                    }
-                    j++;
-                    if(j<len) oddDict[S[j]] = -oddDict[S[j]];
+                    dict.Add(c,1);
                 }
-                oddDict[S[i]] = -oddDict[S[i]];
-            }
-            return count;
-        }
-
-
-
-        public int solution2(string S)
-        {
-
-            var len = S.Length;
-            int count = 0;
-            var oddDict = new Dictionary<char, int>();
-            for (int k = 0; k < 10; k++)
-                oddDict.Add(k.ToString()[0], -1);
-            for (int i = 0; i < len; i++)
-            {
-                oddDict.Clear();
-                for (int k = 0; k < 10; k++)
-                    oddDict.Add(k.ToString()[0], -1);
-                oddDict[S[i]] = 1;
-                int res = 0;
-                for (int j = i; j < len; )
+                else
                 {
-                    res += oddDict[S[j]];
-                    if (res <= 1)
-                    {
-                        if (res == (j - i + 1) % 2)
-                        {
-                            count++;
-                        }
-                    }
-                    j++;
-                    if (j < len) oddDict[S[j]] = -oddDict[S[j]];
+                    dict[c]++;
                 }
-                oddDict[S[i]] = -oddDict[S[i]];
             }
-            return count;
-        }
+            dict.OrderBy(x=>x.Value).ThenBy(y=>y.Key).ToList().ForEach(item=>result.Append(item.Key));
+            return result.ToString();
 
+        }
     }
- }
+}
