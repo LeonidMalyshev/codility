da7e5fe8c07270902d32d4844626c7d3a585c304
Leonid Malyshev <malyshev@streamstar.com>
2016-12-13 09:02:18 +0100

    https://codility.com/programmers/task// silver

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+// https://codility.com/programmers/task//
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp = "54577";
+            var s = new Solution();
+            var sw1 = new Stopwatch();
+            var sw2 = new Stopwatch();
+            sw1.Start();
+            var x1 = s.solution(inp);
+            sw1.Stop();
+            sw2.Start();
+            var x2 = s.solution2(inp);
+            sw2.Stop();
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+            Console.WriteLine(sw1.Elapsed+"    "+sw2.Elapsed);
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(string S)
+        {
+            var len = S.Length;
+            int count = 0;
+            var oddDict = new Dictionary<char,int>();
+            for (int k=0;k<10;k++)
+                oddDict.Add(k.ToString()[0],-1);
+            for (int i = 0; i < len; i++)
+            {
+                oddDict.Clear();
+                for (int k = 0; k < 10; k++)
+                    oddDict.Add(k.ToString()[0], -1);
+                oddDict[S[i]] = 1;
+                int res = 0;
+                for (int j = i; j < len;)
+                {
+                    res += oddDict[S[j]] ;
+                    if (res <= 1)
+                    {
+                        if (res == (j - i + 1)%2)
+                        {
+                            count++;
+                        }
+                    }
+                    j++;
+                    if(j<len) oddDict[S[j]] = -oddDict[S[j]];
+                }
+                oddDict[S[i]] = -oddDict[S[i]];
+            }
+            return count;
+        }
+
+
+
+        public int solution2(string S)
+        {
+
+            var len = S.Length;
+            int count = 0;
+            var oddDict = new Dictionary<char, int>();
+            for (int k = 0; k < 10; k++)
+                oddDict.Add(k.ToString()[0], -1);
+            for (int i = 0; i < len; i++)
+            {
+                oddDict.Clear();
+                for (int k = 0; k < 10; k++)
+                    oddDict.Add(k.ToString()[0], -1);
+                oddDict[S[i]] = 1;
+                int res = 0;
+                for (int j = i; j < len; )
+                {
+                    res += oddDict[S[j]];
+                    if (res <= 1)
+                    {
+                        if (res == (j - i + 1) % 2)
+                        {
+                            count++;
+                        }
+                    }
+                    j++;
+                    if (j < len) oddDict[S[j]] = -oddDict[S[j]];
+                }
+                oddDict[S[i]] = -oddDict[S[i]];
+            }
+            return count;
+        }
+
+    }
+ }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using ConsoleApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var s = new Solution();
+            var rnd = new Random();
+            var sw1 = new Stopwatch();
+            var sw2 = new Stopwatch();
+            for (int i = 0; i < 10; i++)
+            {
+                StringBuilder pat = new StringBuilder();
+                for (int j = rnd.Next(200); j > 0; j--)
+                    pat.Append(rnd.Next(0, 9));
+                sw1.Start();
+                var x1 = s.solution2(pat.ToString());
+                sw1.Stop();
+                sw2.Start();
+                var x2 = s.solution(pat.ToString());
+                sw2.Stop();
+                Assert.AreEqual(x1,x2, pat.ToString());
+            }
+            Console.WriteLine(sw2.Elapsed+"    "+ sw1.Elapsed + "  "+sw1.ElapsedMilliseconds/sw2.ElapsedMilliseconds);
+        }
+        
+    }
+
+}
