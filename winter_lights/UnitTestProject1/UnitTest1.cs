2b9ed6e9e4c51405ee73327d2a2e23ce6567939e
mln141 <ml@mail333.com>
2016-12-21 15:59:02 +0100

    Add files via upload
    37%

---
+++ winter_lights/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using winter_lights;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var rnd = new Random();
+            var sw1 = new Stopwatch();
+            var sw2 = new Stopwatch();
+            for (int i = 0; i < 2; i++)
+            {
+                StringBuilder b= new StringBuilder();
+                for (int j = rnd.Next(1, 2000); j > 0; j--)
+                    b.Append(rnd.Next(9).ToString());
+                var sol = new Solution();
+                int r1, r2;
+                sw1.Restart();
+                r1 = sol.solution(b.ToString());
+                sw1.Stop();
+                sw2.Restart();
+                r2 = sol.solutionold(b.ToString());
+                sw2.Stop();
+                Assert.AreEqual(r1,r2,b.ToString());
+            }
+            Debug.WriteLine(string.Format("{0}  {1} {2}", sw1.Elapsed, sw2.Elapsed, (float)sw2.ElapsedMilliseconds/sw1.ElapsedMilliseconds));
+        }
+    }
+}
---
+++ winter_lights/winter_lights/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace winter_lights
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var rnd = new Random();
+                StringBuilder b = new StringBuilder();
+                for (int j = rnd.Next(5000, 20000); j > 0; j--)
+                    b.Append(rnd.Next(9).ToString());
+                string inp = b.ToString();
+                var s = new Solution();
+                Console.WriteLine(s);
+                Console.WriteLine(s.solution(inp));
+                Console.WriteLine(s.solutionold(inp));
+            }
+        }
+
+        //********* Start of code to submit to codility
+        public class Solution
+        {
+            public int solution(string S)
+            {
+                var len = S.Length;
+                if (len == 0) return 0;
+                if (len == 1) return 1;
+                int result = 1;
+                var dic = new Dictionary<char, int>();
+                for (int i = 0; i < len; i++)
+                    if (dic.ContainsKey(S[i])) dic[S[i]]++;
+                    else dic.Add(S[i], 1);
+                for (int i = 0;i<len - 1;  i++)
+                {
+                    result += mefirst(S.Substring(i), dic);
+                    dic[S[i]]--;
+                } //
+                return result;
+            }
+
+            private int mefirst(string s, Dictionary<char, int> dic)
+            {
+
+                int count = 0;
+                int len = s.Length;
+                var dicOddEven = new Dictionary<char, bool>();
+                foreach (KeyValuePair<char, int> keyValuePair in dic)
+                {
+                    dicOddEven.Add(keyValuePair.Key,keyValuePair.Value%2==0);
+                }
+                int evenOdds = dicOddEven.Values.Count(x => !x);
+                for (int j = len - 1; j >= 0; j--)
+                {
+                    if (evenOdds == (j + 1) % 2)
+                    {
+                        count++;
+                    }
+                    char c = s[j];
+                    if (dicOddEven[c]) evenOdds++;
+                    else evenOdds--;
+                    dicOddEven[c] = !dicOddEven[c];
+                }
+                return count;
+            }
+
+            public int solutionold(string S)
+            {
+                var len = S.Length;
+                if (len == 0) return 0;
+                if (len == 1) return 1;
+                int result = 1;
+                for (int i = len-2 ; i >= 0; i--)
+                    result += mefirstold(S.Substring(i));
+                return result;
+            }
+            private int mefirstold(string s)
+            {
+
+                int count = 0;
+                int len = s.Length;
+                var dic = new Dictionary<char, int>();
+                var dicOddEven = new Dictionary<char, bool>();
+                for (int i = 0; i < len; i++)
+                    if (dic.ContainsKey(s[i])) dic[s[i]]++;
+                    else dic.Add(s[i], 1);
+                foreach (KeyValuePair<char, int> keyValuePair in dic)
+                {
+                    dicOddEven.Add(keyValuePair.Key, keyValuePair.Value % 2 == 0);
+                }
+                int evenOdds = dicOddEven.Values.Count(x => !x);
+                for (int j = len - 1; j >= 0; j--)
+                {
+                    if (evenOdds == (j + 1) % 2)
+                    {
+                        count++;
+                    }
+                    char c = s[j];
+                    //dic[c]--;
+                    if (dicOddEven[c]) evenOdds++;
+                    else evenOdds--;
+                    dicOddEven[c] = !dicOddEven[c];
+                }
+                return count;
+            }
+        }
+    }
